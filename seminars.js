897b65021eb600b68e1a6455c306da618544c94f
Marcos Treviso <marcosvtreviso@gmail.com>
2025-11-20 10:06:38 +0000

    Add some past talks

--- seminars.js
+++ seminars.js
@@ -17,11 +17,85 @@ const SEMINARS = [
   },
 
   // === Past ===
-  // {
-  //   speaker: "...",
-  //   affiliation: "...",
-  //   title: "...",
-  //   date: "2025-01-01",
-  //   status: "past",  // se status for "past" coloca status: "past"
-  // },
+  {
+    speaker: "Vilém Zouhar",
+    affiliation: "ETH Zürich",
+    title: "Quality and Quantity of Machine Translation References for Automated Metrics",
+    date: "2024-01-22",
+    status: "past",
+  },
+  {
+    speaker: "Haoran Xu",
+    affiliation: "Johns Hopkins University",
+    title: "Pushing the Boundaries of LLM Performance in Machine Translation",
+    date: "2024-01-29",
+    status: "past",
+  },
+  {
+    speaker: "Tom Kocmi and Vilém Zouhar",
+    affiliation: "Microsoft and ETH Zürich",
+    title: "Navigating the Metrics Maze: Reconciling Score Magnitudes and Accuracies",
+    date: "2024-02-05",
+    status: "past",
+  },
+  {
+    speaker: "Giorgos Vernikos",
+    affiliation: "EPFL",
+    title: "Don’t Rank, Combine! Combining Machine Translation Hypotheses Using Quality Estimation",
+    date: "2024-02-12",
+    status: "past",
+  },
+  {
+    speaker: "Haikang Deng",
+    affiliation: "UT Austin",
+    title:
+      "Reward-Augmented Decoding: Efficient Controlled Text Generation With a Unidirectional Reward Model",
+    date: "2023-12-18",
+    status: "past",
+  },
+  {
+    speaker: "Prasann Singhal",
+    affiliation: "UT Austin",
+    title:
+      "A Long Way to Go: Investigating Length Correlations in RLHF",
+    date: "2023-11-13",
+    status: "past",
+  },
+  {
+    speaker: "Mara Finkelstein and Markus Freitag",
+    affiliation: "Google Research",
+    title:
+      "MBR and QE Finetuning: Training-time Distillation of the Best and Most Expensive Decoding Methods",
+    date: "2023-11-06",
+    status: "past",
+  },
+  {
+    speaker: "João Sacramento",
+    affiliation: "ETH Zurich",
+    title: "Uncovering mesa-optimization algorithms in Transformers",
+    date: "2023-10-30",
+    status: "past",
+  },
+  {
+    speaker: "Elena Voita",
+    affiliation: "Meta AI",
+    title: "Neurons in Large Language Models",
+    date: "2023-10-23",
+    status: "past",
+  },
+  {
+    speaker: "Tri Dao",
+    affiliation: "Stanford and Together.AI",
+    title:
+      "FlashAttention: Fast and Memory-Efficient Exact Attention with IO-Awareness",
+    date: "2023-10-09",
+    status: "past",
+  },
+  {
+    speaker: "Jason Wei",
+    affiliation: "OpenAI",
+    title: "Scaling, emergence, and reasoning in large language models",
+    date: "2023-09-18",
+    status: "past",
+  },
 ];
